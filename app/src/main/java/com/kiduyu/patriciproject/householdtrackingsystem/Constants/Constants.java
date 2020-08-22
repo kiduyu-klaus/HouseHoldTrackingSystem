6730d567b67b7e2c3e5a6317b51a05e2bc1e33c8
kiduyu-klaus <muigakiduyu1995@gmail.com>
2020-08-22 20:51:04 +0300

    Update Constants.java

--- app/src/main/java/com/kiduyu/patriciproject/householdtrackingsystem/Constants/Constants.java
+++ app/src/main/java/com/kiduyu/patriciproject/householdtrackingsystem/Constants/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final String URL_REGISTER = ROOT_URL + "registerUser.php";
     public static final String URL_LOGIN = ROOT_URL + "userLogin.php";
     public static final String URL_ADD_CONSUMER= ROOT_URL + "addconsumer.php";
+    public static final String CONSUMERBLE_API= ROOT_URL + "consumable_api.php";
 }
